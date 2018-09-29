d272b95eb93bddfed19eb27739167280f8d9101e
Claire Ouellette <claireelisey@gmail.com>
2018-09-29 17:42:22 -0400

    updated submission for node core libraries; installed uuid

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-const http = require('http');
-const port = 3000;
-const fs = require('fs');
+const uuid = require('uuid');
+const newUuid = uuid();
 
-const requestHandler = (request, response) => {
-    fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
-        if (err) throw err;
-        console.log('success')
-    });
-};
+console.log(`your new Universally unique identifier is ${newUuid}`);
 
-const server = http.createServer(requestHandler);
+// you're requiring or importing the my-library module, which you previously exported here
+const myLibrary = require('./my-library');
 
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('You have an error: ${err}');
-    }
-    console.log('server is listening on ${port}');
-});
+// execute my-library here
+myLibrary.myFirstMethod();
